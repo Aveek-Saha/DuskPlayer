0363842e078df5d4196b598d8546ebac554ad1a6
Aveek Saha <aveek.s98@gmail.com>
2018-11-01 18:38:31 +0530

    Version 2.0.0 updates
    Added features and updated UI

--- app/player/player.js
+++ app/player/player.js
@@ -1,16 +1,17 @@
 
-angular.module('Player.player',['ngRoute', 'angular-siri-wave'])
-.config(['$routeProvider', ($routeProvider) => {
-    $routeProvider.when('/player',{
-        templateUrl: 'player/player.html', controller: 'Playerctrl'
+angular.module('Player.player', ['ngRoute', 'angular-siri-wave'])
+  .config(['$routeProvider', ($routeProvider) => {
+    $routeProvider.when('/player', {
+      templateUrl: 'player/player.html', controller: 'Playerctrl'
     })
-}])
-.controller('Playerctrl', ['$scope', '$location', function ($scope, $location) {
+  }])
+  .controller('Playerctrl', ['$scope', '$location', function ($scope, $location) {
     $scope.musicSelected = false;
     $scope.trackName = null;
     $scope.songList = null;
     $scope.songPlaying = false;
     $scope.playListVisible = false;
+    $scope.shuffle = false;
     // $scope.playMusic();
 
     var slider = document.getElementById("myRange");
@@ -18,294 +19,307 @@ angular.module('Player.player',['ngRoute', 'angular-siri-wave'])
 
     const ipc = require('electron').ipcRenderer;
     const jsmediatags = require("jsmediatags");
+    const fs = require('fs')
 
-    ipc.on('modal-file-content', function(event, arg){
-        // console.log(arg)
+    fs.readFile('path.txt', 'utf-8', function (err, buf) {
+      if (err) {
+        return
+      }
+      var temp = [buf.toString()];
+      scanDir(temp);
 
-        if ($scope.songPlaying) {
-          $scope.songPlaying = false;
-          $scope.player.pause();
-        }
-        $scope.songList = arg;
-        console.log($scope.songList)
-        var songArr = [];
-        console.log($scope.songList.files.length)
-        var pth = arg.path;
+      console.log(temp);
 
-        for(let i = 0; i < $scope.songList.files.length; i++){
-            songArr.push({
-                title:arg.path + '/' + $scope.songList.files[i],
-                file:arg.path + '/' + $scope.songList.files[i],
-                name: $scope.songList.files[i],
-                howl: null
-            });
-        }
-
-        $scope.player = new Player(songArr);
-        $scope.musicSelected = true;
-        $scope.$apply()
     });
-    
 
-    $scope.seekToTime = function($event){
-        $scope.player.seek($event.offsetX / sk.offsetWidth);
+    function scanDir(filePath) {
+      if (!filePath || filePath[0] == 'undefined') return;
+
+      fs.readdir(filePath[0], function (err, files) {
+        var arr = [];
+        for (var i = 0; i < files.length; i++) {
+          if (files[i].substr(-4) === '.mp3' || files[i].substr(-4) === '.m4a'
+            || files[i].substr(-5) === '.webm' || files[i].substr(-4) === '.wav'
+            || files[i].substr(-4) === '.aac' || files[i].substr(-4) === '.ogg'
+            || files[i].substr(-5) === '.opus') {
+            arr.push(files[i]);
+          }
+        }
+        // console.log(filePath);
+        var arg = {};
+        arg.files = arr;
+        arg.path = filePath;
+
+        startPlayer(arg)
+
+      })
     }
-    $scope.playPlaylistSong = function(index){
-        $scope.player.skipTo(index);
+
+    ipc.on('selected-files', function (event, arg) {
+      // console.log(arg)
+
+      startPlayer(arg)
+
+    });
+
+    function startPlayer(arg) {
+
+      if ($scope.songPlaying) {
+        $scope.songPlaying = false;
+        $scope.player.pause();
+      }
+      $scope.songList = arg;
+      // console.log($scope.songList)
+      var songArr = [];
+      // console.log($scope.songList.files.length)
+      // var pth = arg.path;
+
+      for (let i = 0; i < $scope.songList.files.length; i++) {
+        songArr.push({
+          title: arg.path + '/' + $scope.songList.files[i],
+          file: arg.path + '/' + $scope.songList.files[i],
+          name: $scope.songList.files[i],
+          howl: null
+        });
+      }
+
+      $scope.player = new Player(songArr);
+      $scope.musicSelected = true;
+      $scope.$apply()
+
+      $scope.playMusic()
     }
-    $scope.nextSong = function(){
+
+    function tag(data) {
+      new jsmediatags.Reader(data.file)
+        .setTagsToRead(["title", "artist", "picture"])
+        .read({
+          onSuccess: function (tag) {
+            if (tag.tags.title) {
+              $scope.trackName = tag.tags.title;
+              $scope.trackArtist = tag.tags.artist;
+            }
+            var image = tag.tags.picture;
+            if (image) {
+              // var pic = document.getElementById('picture')
+              var base64String = "";
+              for (var i = 0; i < image.data.length; i++) {
+                base64String += String.fromCharCode(image.data[i]);
+              }
+              var base64 = "data:image/jpeg;base64," +
+                window.btoa(base64String);
+              document.getElementById('picture').style.display = "block";
+              document.getElementById('picture').setAttribute('src', base64);
+              // pic.style.backgroundImage = "url('" + base64 +"') ";
+            } else {
+              document.getElementById('picture').style.display = "none";
+              // pic.style.backgroundImage = "none";                  
+            }
+          },
+          onError: function (error) {
+            console.log(':(', error.type, error.info);
+          }
+        });
+    }
+
+    $scope.seekToTime = function ($event) {
+      $scope.player.seek($event.offsetX / sk.offsetWidth);
+    }
+    $scope.playPlaylistSong = function (index) {
+      $scope.player.skipTo(index);
+    }
+    $scope.nextSong = function () {
+      if ($scope.shuffle) {
+        $scope.player.skip('random');
+      }
+      else {
         $scope.player.skip('next');
-        $scope.songPlaying=true;
+      }
+      $scope.songPlaying = true;
     }
-    $scope.prevSong = function(){
+    $scope.prevSong = function () {
+      if ($scope.shuffle) {
+        $scope.player.skip('random');
+      }
+      else {
         $scope.player.skip('prev');
-        $scope.songPlaying=true;
+      }
+      $scope.songPlaying = true;
     }
 
-    $scope.showPlaylist = function(){
-        if ($scope.playListVisible) {
-            $scope.playListVisible = false;
-            console.log($scope.playListVisible)            
-        }
-        else{
-            $scope.playListVisible = true;
-            console.log($scope.playListVisible)
-        }
+    $scope.showPlaylist = function () {
+      if ($scope.playListVisible) {
+        $scope.playListVisible = false;
+        // console.log($scope.playListVisible)
+      }
+      else {
+        $scope.playListVisible = true;
+        // console.log($scope.playListVisible)
+      }
     }
 
-    $scope.playMusic = function(){
-        if ($scope.songPlaying) {
-            $scope.songPlaying = false;
-            $scope.player.pause();
-        }
-        else{
-            $scope.songPlaying = true;
-            $scope.player.play();
-        }
+    $scope.playMusic = function () {
+      if ($scope.songPlaying) {
+        $scope.songPlaying = false;
+        $scope.player.pause();
+      }
+      else {
+        $scope.songPlaying = true;
+        $scope.player.play();
+      }
     }
 
-    slider.oninput = function() {
+    $scope.toggleShuffle = function () {
+      if ($scope.shuffle) {
+        $scope.shuffle = false;
+      }
+      else {
+        $scope.shuffle = true;
+      }
+    }
+
+    slider.oninput = function () {
       var val = slider.value / 100;
       $scope.player.volume(val)
     }
 
-    var Player = function(playlist) {
-        this.playlist = playlist;
-        this.index = 0;
+    var Player = function (playlist) {
+      this.playlist = playlist;
+      this.index = 0;
     }
 
     Player.prototype = {
 
-        play: function(index) {
-          var self = this;
-          var sound;
+      play: function (index) {
+        var self = this;
+        var sound;
 
-          index = typeof index === 'number' ? index : self.index;
-          var data = self.playlist[index];
-          $scope.trackName = data.name;
-          $scope.trackArtist = "";
-          console.log(data);
-          new jsmediatags.Reader(data.file)
-            .setTagsToRead(["title", "artist","picture"])
-            .read({
-              onSuccess: function (tag) {
-                if (tag.tags.title){
-                  $scope.trackName = tag.tags.title;
-                  $scope.trackArtist = tag.tags.artist
-                }
-                var image = tag.tags.picture;
-                if (image) {
-                  // var pic = document.getElementById('picture')
-                  var base64String = "";
-                  for (var i = 0; i < image.data.length; i++) {
-                    base64String += String.fromCharCode(image.data[i]);
-                  }
-                  var base64 = "data:image/jpeg;base64," +
-                    window.btoa(base64String);
-                  document.getElementById('picture').style.display = "block";
-                  document.getElementById('picture').setAttribute('src', base64);
-                  // pic.style.backgroundImage = "url('" + base64 +"') ";
-                } else {
-                  document.getElementById('picture').style.display = "none";
-                  // pic.style.backgroundImage = "none";                  
-                }
-              },
-              onError: function (error) {
-                console.log(':(', error.type, error.info);
+        index = typeof index === 'number' ? index : self.index;
+        var data = self.playlist[index];
+        $scope.trackName = data.name;
+        $scope.trackArtist = "";
+        // console.log(data);
+        tag(data);
+
+        if (data.howl) {
+          sound = data.howl;
+        } else {
+          sound = data.howl = new Howl({
+            src: [data.file],
+            html5: true,
+            onplay: function () {
+              $scope.timer = self.formatTime(Math.round(sound.duration()));
+              requestAnimationFrame(self.step.bind(self));
+              $scope.$apply();
+            },
+            onend: function () {
+              if ($scope.shuffle) {
+                self.skip('random');
               }
-            });
-          
-          if (data.howl) {
-            sound = data.howl;
-          } else {
-            sound = data.howl = new Howl({
-              src: [data.file],
-              html5: true,
-              onplay: function() {
-                $scope.timer = self.formatTime(Math.round(sound.duration()));
-                requestAnimationFrame(self.step.bind(self));
-                $scope.$apply();
-              },
-              onend: function() {
+              else {
                 self.skip('right');
               }
-            });
-          }
-
-          sound.play();
-
-          self.index = index;
-        },
-
-        pause: function() {
-          var self = this;
-
-          var sound = self.playlist[self.index].howl;
-
-          sound.pause();
-        },
-
-        skip: function(direction) {
-          var self = this;
-
-          var index = 0;
-          if (direction === 'prev') {
-            index = self.index - 1;
-            if (index < 0) {
-              index = self.playlist.length - 1;
             }
-          } else {
-            index = self.index + 1;
-            if (index >= self.playlist.length) {
-              index = 0;
-            }
-          }
+          });
+        }
 
-          var data = self.playlist[self.index];
-          console.log(data);
-          new jsmediatags.Reader(data.file)
-            .setTagsToRead(["title", "artist", "picture"])
-            .read({
-              onSuccess: function (tag) {
-                if (tag.tags.title) {
-                  $scope.trackName = tag.tags.title;
-                  $scope.trackArtist = tag.tags.artist
-                }
-                else {
-                  $scope.trackName = data.name;
-                  $scope.trackArtist = "";
-                }
-                var image = tag.tags.picture;
-                if (image) {
-                  // var pic = document.getElementById('picture')
-                  var base64String = "";
-                  for (var i = 0; i < image.data.length; i++) {
-                    base64String += String.fromCharCode(image.data[i]);
-                  }
-                  var base64 = "data:image/jpeg;base64," +
-                    window.btoa(base64String);
-                  document.getElementById('picture').style.display = "block";                   
-                  document.getElementById('picture').setAttribute('src', base64);
-                  // pic.style.backgroundImage = "url('" + base64 +"') ";
-                } else {
-                  document.getElementById('picture').style.display = "none";
-                  // pic.style.backgroundImage = "none";                  
-                }
-              },
-              onError: function (error) {
-                console.log(':(', error.type, error.info);
-              }
-            });
+        sound.play();
 
-          self.skipTo(index);
-        },
+        self.index = index;
+      },
 
-        skipTo: function(index) {
-          var self = this;
+      pause: function () {
+        var self = this;
 
-          if (self.playlist[self.index].howl) {
-            // console.log(self.playlist[self.index].howl);
-            self.playlist[self.index].howl.stop();
-          }
+        var sound = self.playlist[self.index].howl;
 
-          var data = self.playlist[index];
-          console.log(data);
-          new jsmediatags.Reader(data.file)
-            .setTagsToRead(["title", "artist", "picture"])
-            .read({
-              onSuccess: function (tag) {
-                if (tag.tags.title) {
-                  $scope.trackName = tag.tags.title;
-                  $scope.trackArtist = tag.tags.artist;
-                }
-                else{
-                  $scope.trackName = data.name;
-                  $scope.trackArtist = "";
-                }
-                var image = tag.tags.picture;
-                if (image) {
-                  // var pic = document.getElementById('picture')
-                  var base64String = "";
-                  for (var i = 0; i < image.data.length; i++) {
-                    base64String += String.fromCharCode(image.data[i]);
-                  }
-                  var base64 = "data:image/jpeg;base64," +
-                    window.btoa(base64String);
-                  document.getElementById('picture').style.display = "block";                    
-                  document.getElementById('picture').setAttribute('src', base64);
-                  // pic.style.backgroundImage = "url('" + base64 +"') ";
-                } else {
-                  document.getElementById('picture').style.display = "none";
-                  // pic.style.backgroundImage = "none";                  
-                }
-              },
-              onError: function (error) {
-                console.log(':(', error.type, error.info);
-              }
-            });
-            if (!$scope.songPlaying) {
-              $scope.songPlaying = true;
-              self.play(index);
-            }
-            else
-              self.play(index);
-        },
+        sound.pause();
+      },
 
-        step: function() {
-          var self = this;
+      skip: function (direction) {
+        var self = this;
 
-          var sound = self.playlist[self.index].howl;
-
-          var seek = sound.seek() || 0;
-          timer.innerHTML = self.formatTime(Math.round(seek));
-          progress.style.width = (((seek / sound.duration()) * 100) || 0) + '%';
-
-          if (sound.playing()) {
-            requestAnimationFrame(self.step.bind(self));
-          }
-        },
-        formatTime: function(secs) {
-          var minutes = Math.floor(secs / 60) || 0;
-          var seconds = (secs - minutes * 60) || 0;
-
-          return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-        },
-        volume: function(val) {
-          var self = this;
-      
-          // Update the global volume (affecting all Howls).
-          Howler.volume(val);
-      
-        },
-        seek: function(time) {
-          var self = this;
-
-          var sound = self.playlist[self.index].howl;
-
-          if (sound.playing() || true) {
-            sound.seek(sound.duration() * time);
-            requestAnimationFrame(self.step.bind(self));
+        var index = 0;
+        if (direction === 'prev') {
+          index = self.index - 1;
+          if (index < 0) {
+            index = self.playlist.length - 1;
           }
         }
+        else if (direction === 'random') {
+          index = Math.floor(Math.random() * self.playlist.length) + 1;
+          console.log(index);
+
+        }
+        else {
+          index = self.index + 1;
+          if (index >= self.playlist.length) {
+            index = 0;
+          }
+        }
+
+        var data = self.playlist[self.index];
+        console.log(data);
+        tag(data);
+
+        self.skipTo(index);
+      },
+
+      skipTo: function (index) {
+        var self = this;
+
+        if (self.playlist[self.index].howl) {
+          // console.log(self.playlist[self.index].howl);
+          self.playlist[self.index].howl.stop();
+        }
+
+        var data = self.playlist[index];
+        console.log(data);
+        tag(data);
+
+        if (!$scope.songPlaying) {
+          $scope.songPlaying = true;
+          self.play(index);
+        }
+        else
+          self.play(index);
+      },
+
+      step: function () {
+        var self = this;
+
+        var sound = self.playlist[self.index].howl;
+
+        var seek = sound.seek() || 0;
+        timer.innerHTML = self.formatTime(Math.round(seek));
+        progress.style.width = (((seek / sound.duration()) * 100) || 0) + '%';
+
+        if (sound.playing()) {
+          requestAnimationFrame(self.step.bind(self));
+        }
+      },
+      formatTime: function (secs) {
+        var minutes = Math.floor(secs / 60) || 0;
+        var seconds = (secs - minutes * 60) || 0;
+
+        return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+      },
+      volume: function (val) {
+        var self = this;
+
+        // Update the global volume (affecting all Howls).
+        Howler.volume(val);
+
+      },
+      seek: function (time) {
+        var self = this;
+
+        var sound = self.playlist[self.index].howl;
+
+        if (sound.playing() || true) {
+          sound.seek(sound.duration() * time);
+          requestAnimationFrame(self.step.bind(self));
+        }
       }
-}])
+    }
+  }])
--- main.js
+++ main.js
@@ -1,81 +1,107 @@
-const {app, BrowserWindow, dialog, Menu} = require('electron')
-  const path = require('path')
-  const url = require('url')
+const { app, BrowserWindow, dialog, Menu } = require('electron')
+const path = require('path')
+const url = require('url')
 
-const fs = require('fs')  
-  
-  let win
-  
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({ width: 900, height: 550, icon: __dirname + '/music-player.ico'})
+const fs = require('fs')
 
-    var menu = Menu.buildFromTemplate([
-      {
-          label: 'Folders',
-          accelerator: 'CommandOrControl+o',
-          click: function(){
-                openFolderDialog()
-              }
-      },
-      {
-          label: 'Info'
+let win
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 1000, 
+    height: 620, 
+    icon: __dirname + '/dusk.png' })
+
+  var menu = Menu.buildFromTemplate([
+    {
+      label: 'Folders',
+      accelerator: 'CommandOrControl+o',
+      click: function () {
+        openFolderDialog()
       }
+    },
+    {
+      label: 'Info'
+    }
   ])
-    Menu.setApplicationMenu(menu)
-  
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'app/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-  
-    // Open the DevTools.
-    // win.webContents.openDevTools()
-  
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-      win = null
-    })
-  }
-  
-  app.on('ready', createWindow)
-  
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-  
-  app.on('activate', () => {
-    if (win === null) {
-      createWindow()
-    }
-  })
+  Menu.setApplicationMenu(menu)
 
-function openFolderDialog(){
-  dialog.showOpenDialog(win,{
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'app/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // fs.readFile('path.txt', 'utf-8', function (err, buf) {
+  //   if (err) {
+  //     return
+  //   }
+  //   var temp = [buf.toString()];
+  //   scanDir(temp);
+
+  // });
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+function openFolderDialog() {
+  dialog.showOpenDialog(win, {
     properties: ['openDirectory']
-  },function(filePath){
-    if(!filePath) return;
-    fs.readdir(filePath[0], function (err, files){
-      var arr = [];
-      for(var i = 0; i < files.length; i++){
-        if (files[i].substr(-4) === '.mp3' || files[i].substr(-4) === '.m4a' 
-          || files[i].substr(-4) === '.webm' || files[i].substr(-4) === '.wav'
-          || files[i].substr(-4) === '.aac' || files[i].substr(-4) === '.ogg'
-          || files[i].substr(-4) === '.opus') {
-          arr.push(files[i]);
-        }
+  }, function (filePath) {
+
+    if (filePath) {
+      fs.writeFile('path.txt', filePath, function (err, data) {
+        if (err) console.log(err);
+      });
+      // console.log(filePath);
+
+      scanDir(filePath)
+    }
+  })
+}
+
+function scanDir(filePath) {
+  if (!filePath || filePath[0] == 'undefined') return;
+
+  fs.readdir(filePath[0], function (err, files) {
+    var arr = [];
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].substr(-4) === '.mp3' || files[i].substr(-4) === '.m4a'
+        || files[i].substr(-5) === '.webm' || files[i].substr(-4) === '.wav'
+        || files[i].substr(-4) === '.aac' || files[i].substr(-4) === '.ogg'
+        || files[i].substr(-5) === '.opus') {
+        arr.push(files[i]);
       }
-      console.log(arr);
-      var objToSend = {};
-      objToSend.files = arr;
-      objToSend.path = filePath;
-      win.webContents.send('modal-file-content', objToSend)
-    })
-    
+    }
+    // console.log(filePath);
+    var objToSend = {};
+    objToSend.files = arr;
+    objToSend.path = filePath;
+
+    win.webContents.send('selected-files', objToSend)
+    // console.log(win.webContents);
+
   })
 }
