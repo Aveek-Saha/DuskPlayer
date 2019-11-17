f4c3d09e78eda702b85d24a33523e1b12f91d738
CodeF0x <contact@codef0x.dev>
2019-11-17 05:25:49 +0100

    Proper menu on Mac.

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const { app, BrowserWindow, dialog, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
-const join = require('path').join;
-const { autoUpdater } = require("electron-updater");
+const join = require('path').join
+const { autoUpdater } = require('electron-updater')
 const fs = require('fs')
-const openAboutWindow = require('about-window').default;
-const storage = require('electron-json-storage');
+const openAboutWindow = require('about-window').default
+const storage = require('electron-json-storage')
 
-const dataPath = storage.getDataPath();
+const dataPath = storage.getDataPath()
 
 let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ 
-    width: 1000, 
+  win = new BrowserWindow({
+    width: 1000,
     height: 620,
     icon: __dirname + '/dusk.png',
     webPreferences: {
@@ -26,8 +26,6 @@ function createWindow() {
   var dark = false
   var disco = false
 
-
-
   // fs.readFile('theme.txt', 'utf-8', function (err, buf) {
   //   if (err)
   //     return
@@ -37,65 +35,107 @@ function createWindow() {
   //   // console.log(temp);
   // });
 
-  storage.has('theme', function (error, hasKey) {
-    if (error) throw error;
+  storage.has('theme', function(error, hasKey) {
+    if (error) throw error
     if (hasKey) {
-      storage.get('theme', function (error, data) {
-        if (error) throw error;
-        console.log(data.theme);
-        if (data.theme == "light")
-          light = true
-        else if (data.theme == "disco")
-          disco = true
-        else
-          dark = true
-      });
+      storage.get('theme', function(error, data) {
+        if (error) throw error
+        console.log(data.theme)
+        if (data.theme == 'light') light = true
+        else if (data.theme == 'disco') disco = true
+        else dark = true
+      })
     }
-  });
+  })
 
   function handleClick(menuItem, browserWindow, event) {
-    console.log(menuItem.label.toLowerCase());
-    storage.set('theme', { theme: menuItem.label.toLowerCase() }, function (error) {
-      if (error) throw error;
-    });
+    console.log(menuItem.label.toLowerCase())
+    storage.set('theme', { theme: menuItem.label.toLowerCase() }, function(
+      error
+    ) {
+      if (error) throw error
+    })
   }
 
-  var menu = Menu.buildFromTemplate([
-    {
-      label: 'Folders',
-      accelerator: 'CommandOrControl+o',
-      click: function () {
-        openFolderDialog()
-      }
-    },
-    {
-      label: 'Theme',
-      submenu:[
-        { label: 'Light', type: 'radio', click: handleClick, checked: light },
-        { label: 'Dark', type: 'radio', click: handleClick, checked: dark },
-        { label: 'Disco', type: 'radio', click: handleClick, checked: disco }
-      ]
-    },
-    {
-      label: 'Info',
-        click: function () {
-            openAboutWindow({ 
-              product_name: "Dusk Player",
-              homepage: "https://home.aveek.io",
-              copyright: "By Aveek Saha", 
-              icon_path: join(__dirname, 'build/icon.png'),
-            })
-        }
+  /**
+   * Because menu buttons on MacOS *require* at least one submenu,
+   * store them in variables inorder to modify them if application is
+   * running on Mac.
+   */
+  var openFolder = {
+    label: 'Folders',
+    accelerator: 'CommandOrControl+o',
+    click: function() {
+      openFolderDialog()
     }
-  ])
-  Menu.setApplicationMenu(menu)
+  }
+
+  var info = {
+    label: 'Info',
+    click: function() {
+      openAboutWindow({
+        product_name: 'Dusk Player',
+        homepage: 'https://home.aveek.io',
+        copyright: 'By Aveek Saha',
+        icon_path: join(__dirname, 'build/icon.png')
+      })
+    }
+  }
+
+  var theme = {
+    label: 'Theme',
+    submenu: [
+      { label: 'Light', type: 'radio', click: handleClick, checked: light },
+      { label: 'Dark', type: 'radio', click: handleClick, checked: dark },
+      { label: 'Disco', type: 'radio', click: handleClick, checked: disco }
+    ]
+  }
+
+  if (process.platform === 'darwin') {
+    openFolder = {
+      label: 'Folders',
+      submenu: [
+        {
+          label: 'Open folder',
+          accelerator: 'CommandOrControl+o',
+          click: function() {
+            openFolderDialog()
+          }
+        }
+      ]
+    }
+
+    info = {
+      label: 'Info',
+      submenu: [
+        {
+          label: 'Show info',
+
+          click: function() {
+            openAboutWindow({
+              product_name: 'Dusk Player',
+              homepage: 'https://home.aveek.io',
+              copyright: 'By Aveek Saha',
+              icon_path: join(__dirname, 'build/icon.png')
+            })
+          }
+        }
+      ]
+    }
+
+    createMenuMac(openFolder, theme, info)
+  } else {
+    createMenuOther(openFolder, theme, info)
+  }
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'app/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'app/index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  )
 
   // fs.readFile('path.txt', 'utf-8', function (err, buf) {
   //   if (err) {
@@ -106,14 +146,14 @@ function createWindow() {
 
   // });
 
-  storage.has('path', function (error, hasKey) {
-    if (error) throw error;
+  storage.has('path', function(error, hasKey) {
+    if (error) throw error
     if (hasKey) {
-      storage.get('path', function (error, data) {
-        if (error) throw error;
+      storage.get('path', function(error, data) {
+        if (error) throw error
 
-        scanDir([data.path.toString()]);
-      });
+        scanDir([data.path.toString()])
+      })
     }
   })
 
@@ -128,7 +168,7 @@ function createWindow() {
 
 app.on('ready', () => {
   createWindow()
-  autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdatesAndNotify()
 })
 
 // Quit when all windows are closed.
@@ -145,53 +185,82 @@ app.on('activate', () => {
 })
 
 function openFolderDialog() {
-  dialog.showOpenDialog(win, {
-    properties: ['openDirectory']
-  }, function (filePath) {
+  dialog.showOpenDialog(
+    win,
+    {
+      properties: ['openDirectory']
+    },
+    function(filePath) {
+      if (filePath) {
+        // fs.writeFile('path.txt', filePath, function (err, data) {
+        //   if (err) console.log(err);
+        // });
+        // console.log(walkSync(filePath[0]));
 
-    if (filePath) {
-      // fs.writeFile('path.txt', filePath, function (err, data) {
-      //   if (err) console.log(err);
-      // });
-      // console.log(walkSync(filePath[0]));
+        storage.set('path', { path: filePath }, function(error) {
+          if (error) throw error
+        })
 
-      storage.set('path', { path: filePath }, function (error) {
-        if (error) throw error;
-      });
-
-      scanDir(filePath)
+        scanDir(filePath)
+      }
     }
-  })
+  )
 }
 
 var walkSync = function(dir, filelist) {
-  files = fs.readdirSync(dir);
-  filelist = filelist || [];
+  files = fs.readdirSync(dir)
+  filelist = filelist || []
   files.forEach(function(file) {
     if (fs.statSync(path.join(dir, file)).isDirectory()) {
-      filelist = walkSync(path.join(dir, file), filelist);
-    }
-    else {
-      if (file.substr(-4) === '.mp3' || file.substr(-4) === '.m4a'
-        || file.substr(-5) === '.webm' || file.substr(-4) === '.wav'
-        || file.substr(-4) === '.aac' || file.substr(-4) === '.ogg'
-        || file.substr(-5) === '.opus') {
-        filelist.push(path.join(dir, file));
+      filelist = walkSync(path.join(dir, file), filelist)
+    } else {
+      if (
+        file.substr(-4) === '.mp3' ||
+        file.substr(-4) === '.m4a' ||
+        file.substr(-5) === '.webm' ||
+        file.substr(-4) === '.wav' ||
+        file.substr(-4) === '.aac' ||
+        file.substr(-4) === '.ogg' ||
+        file.substr(-5) === '.opus'
+      ) {
+        filelist.push(path.join(dir, file))
       }
     }
-  });
-  return filelist;
-};
+  })
+  return filelist
+}
 
 function scanDir(filePath) {
-  if (!filePath || filePath[0] == 'undefined') return;
+  if (!filePath || filePath[0] == 'undefined') return
 
-  var arr = walkSync(filePath[0]);
+  var arr = walkSync(filePath[0])
 
-  var objToSend = {};
-  objToSend.files = arr;
-  objToSend.path = filePath;
+  var objToSend = {}
+  objToSend.files = arr
+  objToSend.path = filePath
 
   win.webContents.send('selected-files', objToSend)
+}
 
+function createMenuOther(openFolder, theme, info) {
+  var menu = Menu.buildFromTemplate([openFolder, theme, info])
+  Menu.setApplicationMenu(menu)
+}
+
+function createMenuMac(openFolder, theme, info) {
+  var menu = Menu.buildFromTemplate([
+    {
+      label: require('electron').app.getName(),
+      submenu: [
+        {
+          role: 'quit',
+          accelerator: 'Cmd+Q'
+        }
+      ]
+    },
+    openFolder,
+    theme,
+    info
+  ])
+  Menu.setApplicationMenu(menu)
 }
